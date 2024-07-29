56a78ec229c0cedfc3612df3731da8fe23b2f9ba
Farhan Bukhari <bfarhan03@gmail.com>
2024-07-29 11:51:00 +0530

    initial commit

---
+++ JavaStrings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class JavaStrings{
+    public static void main(String[] args) {
+        String s = new String("Farhan");
+        String name = "Farhan";
+        String combinedName = name+s;
+    }
+}
